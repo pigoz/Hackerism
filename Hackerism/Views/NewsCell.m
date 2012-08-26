f41312ff8ae759b9b15edfd109b918a1ef59f356
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2012-08-26 23:34:01 +0200

    gradient on cell

--- Hackerism/Views/NewsCell.m
+++ Hackerism/Views/NewsCell.m
@@ -11,4 +11,28 @@
     NSArray *nibFileEntries = [[NSBundle mainBundle] loadNibNamed:@"NewsCell" owner:nil options:nil];
     return nibFileEntries[0];
 }
+
+- (void)drawRect:(CGRect)rect
+{
+    CGContextRef currentContext = UIGraphicsGetCurrentContext();
+
+    CGGradientRef glossGradient;
+    CGColorSpaceRef rgbColorspace;
+    size_t num_locations = 2;
+    CGFloat locations[2] = { 0.0, 1.0 };
+    CGFloat components[8] = {
+        1.0, 1.0, 1.0, 1.0,  // Start color
+        0.933, 0.933, 0.933, 1.0 }; // End color
+
+    rgbColorspace = CGColorSpaceCreateDeviceRGB();
+    glossGradient = CGGradientCreateWithColorComponents(rgbColorspace, components, locations, num_locations);
+
+    CGRect currentBounds = self.bounds;
+    CGPoint topCenter = CGPointMake(CGRectGetMidX(currentBounds), 0.0f);
+    CGPoint lowCenter = CGPointMake(CGRectGetMidX(currentBounds), CGRectGetHeight(currentBounds));
+    CGContextDrawLinearGradient(currentContext, glossGradient, topCenter, lowCenter, 0);
+
+    CGGradientRelease(glossGradient);
+    CGColorSpaceRelease(rgbColorspace);
+}
 @end
